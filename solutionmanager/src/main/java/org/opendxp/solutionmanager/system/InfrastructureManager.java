188d9bd33ab87e290e61d20b111f7bfbfcca0bd6
maik <moewes@users.noreply.github.com>
2018-12-10 06:54:27 +0100

    [closes 2] integrating vaadin

---
+++ solutionmanager/src/main/java/org/opendxp/solutionmanager/system/InfrastructureManager.java
@@ -0,0 +1,12 @@
+package org.opendxp.solutionmanager.system;
+
+import javax.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class InfrastructureManager {
+
+  public String foo() {
+    return" foo";
+  }
+
+}
