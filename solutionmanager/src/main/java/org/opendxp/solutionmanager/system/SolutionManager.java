4a0a73c3703278a33ba1af24c74c1ee15c19a861
maik <moewes@users.noreply.github.com>
2018-12-11 07:02:48 +0100

    [closes 2] integrating vaadin

--- solutionmanager/src/main/java/org/opendxp/solutionmanager/system/SolutionManager.java
+++ solutionmanager/src/main/java/org/opendxp/solutionmanager/system/SolutionManager.java
@@ -1,11 +1,20 @@
 package org.opendxp.solutionmanager.system;
 
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
 import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
 
 @ApplicationScoped
 public class SolutionManager {
 
+  @Inject
+  @ConfigProperty(name="userId", defaultValue="me")
+  String userId;
+
   public String getVersion() {
     return "development 0.1.0";
   }
+
+  public String getUserId() { return userId; }
 }
--- solutionmanager/src/main/java/org/opendxp/solutionmanager/vaadin/DockerTestView.java
+++ solutionmanager/src/main/java/org/opendxp/solutionmanager/vaadin/DockerTestView.java
@@ -1,11 +1,16 @@
 package org.opendxp.solutionmanager.vaadin;
 
 import com.github.dockerjava.api.model.Network;
+import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.HasComponents;
 import com.vaadin.flow.component.Text;
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.tabs.Tab;
+import com.vaadin.flow.component.tabs.Tabs;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.ParentLayout;
 import com.vaadin.flow.router.Route;
@@ -23,12 +28,14 @@ public class DockerTestView extends VerticalLayout  {
   @Inject
   private SolutionManager sm;
 
+  private VerticalLayout tabContent = new VerticalLayout();
+
   @Inject
   public DockerTestView(DockerMaschine dm) {
 
 
     Button button = new Button("Test",
-            event -> Notification.show("Hello from openDXP Solutionmanager - Version: " + sm.getVersion() + dm.getInfo()));
+            event -> Notification.show("Hello from openDXP Solutionmanager - Version: " + sm.getVersion() + dm.getInfo() + sm.getUserId()));
     add(button);
     Text text = new Text(dm.getInfo());
     add(text);
@@ -38,13 +45,51 @@ public class DockerTestView extends VerticalLayout  {
     menu.add(new RouterLink("Greeting", MainView.class));
     add(menu);
 
-    List<Network> networks = dm.getNetworks();
+    Tab tab1 = new Tab("Networks");
+    Tab tab2 = new Tab("Tab two");
+    Tab tab3 = new Tab("Tab three");
+    Tabs tabs = new Tabs(tab1, tab2, tab3);
+    tabs.setFlexGrowForEnclosedTabs(1);
 
-    for (Network network : networks) {
-      Div entry = new Div();
-      entry.setText(network.getId() + " " + network.getName() + " " + network.getOptions().toString());
-      add(entry);
-    }
+    tabs.addSelectedChangeListener(event -> {
+
+      tabContent.removeAll();
+      switch (tabs.getSelectedIndex()) {
+        case 0:
+          addNetworksToTabContent(tabContent,dm);
+          break;
+        case 1:
+          Div a = new Div();
+          a.setText("Page 2");
+          tabContent.add(a);
+          break;
+        case 2:
+          Div b = new Div();
+          b.setText("Page 3");
+          tabContent.add(b);
+      }
+    });
+
+    add(tabs);
+
+    addNetworksToTabContent(tabContent,dm);
+
+    add(tabContent);
+
   }
+
+  private void addNetworksToTabContent(HasComponents parent, DockerMaschine dm) {
+
+    Grid<Network> grid = new Grid<>();
+
+    grid.setItems(dm.getNetworks());
+
+    grid.addColumn(Network::getName).setHeader("Name");
+    grid.addColumn(Network::getOptions).setHeader("Options");
+
+    parent.add(grid);
+  }
+
+
 
 }
