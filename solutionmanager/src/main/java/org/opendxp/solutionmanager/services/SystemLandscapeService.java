e574e23c9df5441ea4b0910ff9f27d84c9d3d59a
maik <moewes@users.noreply.github.com>
2018-12-29 14:25:00 +0100

    [closes 2] first views

--- solutionmanager/src/main/java/org/opendxp/solutionmanager/services/SystemLandscapeService.java
+++ solutionmanager/src/main/java/org/opendxp/solutionmanager/services/SystemLandscapeService.java
@@ -1,9 +1,12 @@
 package org.opendxp.solutionmanager.services;
 
+import org.yaml.snakeyaml.Yaml;
+
 import javax.ejb.Lock;
 import javax.ejb.LockType;
 import javax.ejb.Singleton;
 import javax.ejb.Stateless;
+import java.io.*;
 
 @Singleton
 public class SystemLandscapeService {
@@ -14,8 +17,22 @@ public class SystemLandscapeService {
   public SystemLandscape getSystemLandscape() {
 
     if (systemLandscape == null) {
-      systemLandscape = new SystemLandscape();
-      systemLandscape.setName("New Instance");
+      
+      File file = new File("./newfile.txt");
+      
+      if (file.exists()) {
+        
+        try {
+          FileInputStream fip = new FileInputStream(file);
+          Yaml yaml = new Yaml();
+          systemLandscape = yaml.load(fip);
+        } catch (FileNotFoundException e) {
+          e.printStackTrace();
+        }
+      } else {
+        systemLandscape = new SystemLandscape();
+        systemLandscape.setName("New Instance");
+      }
     }
 
     return systemLandscape;
@@ -27,5 +44,20 @@ public class SystemLandscapeService {
     this.systemLandscape = systemLandscape;
 
     this.systemLandscape.setName(systemLandscape.getName() + "!");
+
+
+    Yaml yaml = new Yaml();
+    File file = new File("./newfile.txt");
+    try (FileOutputStream fop = new FileOutputStream(file)) {
+
+      String s = yaml.dumpAsMap(systemLandscape);
+      fop.write(s.getBytes());
+      fop.flush();
+
+    } catch (FileNotFoundException e) {
+      e.printStackTrace();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
   }
 }
